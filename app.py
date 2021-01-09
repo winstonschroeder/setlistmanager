5377625161c4683262e7bf657755e8f68f72fadb
ruben <info@rubenfdc.net>
2021-01-10 00:45:51 +0100

    still having trouble drawing the scenes... check "App.scenes.append" section for errors

---
+++ app.py
@@ -0,0 +1,122 @@
+import pygame
+from pygame.locals import *
+
+class Text:
+    """Create a text object."""
+
+    def __init__(self, text, pos, **options):
+        self.text = text
+        self.pos = pos
+
+        self.fontname = None
+        self.fontsize = 72
+        self.fontcolor = Color('black')
+        self.set_font()
+        self.render()
+
+    def set_font(self):
+        """Set the font from its name and size."""
+        self.font = pygame.font.Font(self.fontname, self.fontsize)
+
+    def render(self):
+        """Render the text into an image."""
+        self.img = self.font.render(self.text, True, self.fontcolor)
+        self.rect = self.img.get_rect()
+        self.rect.topleft = self.pos
+
+    def draw(self):
+        """Draw the text image to the screen."""
+        App.screen.blit(self.img, self.rect)
+
+class Scene:
+    """Create a new scene (room, level, view)."""
+    id = 0
+    bg = Color('gray')
+
+    def __init__(self, *args, **kwargs):
+        # Set the instance id and increment the class id
+        self.id = Scene.id
+        Scene.id += 1
+        self.nodes = []
+        self.bg = Scene.bg
+
+        # Append the new scene and make it the current scene
+        App.scenes.append(self)
+        App.scene = self
+
+    def draw(self):
+        """Draw all objects in the scene."""
+        App.screen.fill(self.bg)
+        for node in self.nodes:
+            node.draw()
+        pygame.display.flip()
+
+    def __str__(self):
+        return 'Scene {}'.format(self.id)
+
+class App:
+    """Create a single-window app with multiple scenes."""
+
+    def __init__(self):
+        """Initialize pygame and the application."""
+        pygame.init()
+
+        self.shortcuts = {
+            (K_x, KMOD_LMETA): 'print("cmd+X")',
+            (K_x, KMOD_LALT): 'print("alt+X")',
+            (K_x, KMOD_LCTRL): 'print("ctrl+X")',
+            (K_x, KMOD_LMETA + KMOD_LSHIFT): 'print("cmd+shift+X")',
+            (K_x, KMOD_LMETA + KMOD_LALT): 'print("cmd+alt+X")',
+            (K_x, KMOD_LMETA + KMOD_LALT + KMOD_LSHIFT): 'print("cmd+alt+shift+X")',
+            (K_F11): 'self.toggle_fullscreen()',
+            (K_r, KMOD_LMETA): 'self.toggle_resizable()',
+            (K_g, KMOD_LMETA): 'self.toggle_frame()',
+        }
+        self.flags = RESIZABLE
+        self.rect = Rect(0, 0, 640, 240)
+        App.scenes = []
+        App.screen = pygame.display.set_mode(self.rect.size, self.flags)
+        # App.t = Text('Pygame App', pos=(20, 20))
+
+        App.running = True
+
+    def run(self):
+        """Run the main event loop."""
+        while App.running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    App.running = False
+                if event.type == KEYDOWN:
+                    self.do_shortcut(event)
+
+            #App.screen.fill(Color('gray'))
+            #App.t.draw()
+            pygame.display.update()
+
+        pygame.quit()
+
+    def do_shortcut(self, event):
+        """Find the the key/mod combination in the dictionary and execute the cmd."""
+        k = event.key
+        m = event.mod
+        if (k, m) in self.shortcuts:
+            exec(self.shortcuts[k, m])
+
+    def toggle_fullscreen(self):
+        """Toggle between full screen and windowed screen."""
+        self.flags ^= FULLSCREEN
+        pygame.display.set_mode((0, 0), self.flags)
+
+    def toggle_resizable(self):
+        """Toggle between resizable and fixed-size window."""
+        self.flags ^= RESIZABLE
+        pygame.display.set_mode(self.rect.size, self.flags)
+
+    def toggle_frame(self):
+        """Toggle between frame and noframe window."""
+        self.flags ^= NOFRAME
+        pygame.display.set_mode(self.rect.size, self.flags)
+
+
+if __name__ == '__main__':
+    App().run()
--- setlistmanager.py
+++ setlistmanager.py
@@ -1,75 +1,24 @@
-import pygame
-from pygame.locals import *
+from app import *
 
-class Text:
-    """Create a text object."""
-
-    def __init__(self, text, pos, **options):
-        self.text = text
-        self.pos = pos
-
-        self.fontname = None
-        self.fontsize = 72
-        self.fontcolor = Color('black')
-        self.set_font()
-        self.render()
-
-    def set_font(self):
-        """Set the font from its name and size."""
-        self.font = pygame.font.Font(self.fontname, self.fontsize)
-
-    def render(self):
-        """Render the text into an image."""
-        self.img = self.font.render(self.text, True, self.fontcolor)
-        self.rect = self.img.get_rect()
-        self.rect.topleft = self.pos
-
-    def draw(self):
-        """Draw the text image to the screen."""
-        App.screen.blit(self.img, self.rect)
-
-class App:
-    """Create a single-window app with multiple scenes."""
-
+class Demo(App):
     def __init__(self):
-        """Initialize pygame and the application."""
-        pygame.init()
-        self.shortcuts = {
-            (K_x, KMOD_LMETA): 'print("cmd+X")',
-            (K_x, KMOD_LALT): 'print("alt+X")',
-            (K_x, KMOD_LCTRL): 'print("ctrl+X")',
-            (K_x, KMOD_LMETA + KMOD_LSHIFT): 'print("cmd+shift+X")',
-            (K_x, KMOD_LMETA + KMOD_LALT): 'print("cmd+alt+X")',
-            (K_x, KMOD_LMETA + KMOD_LALT + KMOD_LSHIFT): 'print("cmd+alt+shift+X")',
-        }
-        flags = RESIZABLE
-        App.screen = pygame.display.set_mode((640, 240), flags)
-        App.t = Text('Pygame App', pos=(20, 20))
+        super().__init__()
 
-        App.running = True
+        Scene(caption='Intro')
+        Text('Scene 0', pos=(10, 20))
+        Text('Introduction screen the app', pos=(20, 20))
 
-    def run(self):
-        """Run the main event loop."""
-        while App.running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    App.running = False
-                if event.type == KEYDOWN:
-                    self.do_shortcut(event)
+        Scene(bg=Color('yellow'), caption='Options')
+        Text('Scene 1', pos=(10, 20))
+        Text('Option screen of the app', pos=(20, 20))
 
-            App.screen.fill(Color('gray'))
-            App.t.draw()
-            pygame.display.update()
+        Scene(bg=Color('green'), caption='Main')
+        Text('Scene 2', pos=(10, 20))
+        Text('Main screen of the app', pos=(20, 20))
 
-        pygame.quit()
+        App.scene = App.scenes[1]
 
-    def do_shortcut(self, event):
-        """Find the the key/mod combination in the dictionary and execute the cmd."""
-        k = event.key
-        m = event.mod
-        if (k, m) in self.shortcuts:
-            exec(self.shortcuts[k, m])
 
 
 if __name__ == '__main__':
-    App().run()
+    Demo().run()

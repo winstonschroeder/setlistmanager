d2c6246d8ce5c38298aa7f9a8ef4d94f8529b2e2
ruben <info@rubenfdc.net>
2021-01-09 23:25:38 +0100

    replace simple gui and try to use raspberry in parallel

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='setlistmanager',
+    version='0.0.1',
+    packages=[''],
+    url='https://rubenfdc.net',
+    license='',
+    author='Ruben Fernandez del Campo',
+    author_email='info@rubenfdc.net',
+    description='Run through setlists and songs with a single footswitch.'
+)
